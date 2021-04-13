80eaa48a005c51d5ad86df37f75166a2239a3507
Rigoberto Caionda <rigobertocaionda98@gmail.com>
2021-04-13 14:41:02 +0100

    Adding phrases

--- assets/js/frases.js
+++ assets/js/frases.js
@@ -5,5 +5,12 @@ let frases = [
 	},
 	{
 		frase: 'Não seja como o pato que nada, voa e anda, mas não faz nada de maneira eficiente!'
+	},
+	{
+		frase: 'Faça o oposto do que a sua ansiedade quer!'
+	},
+	{
+		frase: 'Se você não tem 20 minutos do seu dia para cuidar de você~, então você precisa de umas duas horas',
+		autor: 'Felipe Marx'
 	}
 ];
